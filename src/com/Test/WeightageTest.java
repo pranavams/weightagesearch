e99810142814dd121e2ed1f7c9a476f0b641f1d7
Pranav <pranavam.s@gmail.com>
2018-11-21 11:23:24 +0530

    Initial Version from Venkat

---
+++ src/com/Test/WeightageTest.java
@@ -0,0 +1,78 @@
+package com.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.main.Weightage;
+
+public class WeightageTest {
+	
+	private Weightage weightage;
+	
+	@Before
+	public void setup(){
+		weightage  = new Weightage();
+	}
+	
+	@Test
+	public void whether_Page_Map_Initializied() {
+		Map<String, String[]> pagingStored = weightage.isPagingStored();
+		
+		assertNotNull(weightage);
+		assertEquals(5, pagingStored.size());
+		
+	}
+	
+	@Test
+	public void check_isElement_Available_inSingleNode() {
+		String[] checkerElement = {"Automobile"};
+		String checkElement = weightage.checkElement(checkerElement);
+		assertEquals("P4", checkElement);
+	}
+	
+	@Test
+	public void check_isElement_Available_inMultiNode() {
+		String[] checkerElement = {"Review"};
+		String checkElement = weightage.checkElement(checkerElement);
+		assertEquals("P1P2P3", checkElement);
+	}
+	
+	@Test
+	public void isElement_UnAvailable_inNode() {
+		String[] checkerElement = {"Maddy"};
+		String checkElement = weightage.checkElement(checkerElement);
+		assertEquals("No Records Available", checkElement);
+	}
+	
+	@Test
+	public void isElement_Fetched_AlongwithIndex(){
+		String[] checkerElement = {"Showroom"};
+		String checkElement = weightage.checkElement(checkerElement);
+		assertEquals("P2", checkElement);
+	}
+	
+	@Test
+	public void isGivenElements_Combination_Splitup_Available_LeastNodes() {
+		String[] checkerElement = {"Ford","Automobile"};
+		String checkElement = weightage.checkElement(checkerElement);
+		assertEquals("P1P4P5", checkElement);
+	}
+	
+	@Test
+	public void isGivenElements_Combination_Splitup_Available_ManyNodes() {
+		String[] checkerElement = {"Ford","Review"};
+		String checkElement = weightage.checkElement(checkerElement);
+		assertEquals("P1P2P3P5", checkElement);
+	}
+	
+	
+	
+
+	
+
+}
---
+++ src/com/main/Weightage.java
@@ -0,0 +1,41 @@
+package com.main;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Weightage {
+
+	public Map<String, String[]> isPagingStored() {
+		Map<String, String[]> map = new HashMap();
+		String[] arr1 = { "Ford", "Car", "Review" };
+		String[] arr2 = { "Toyota", "Showroom", "Review" };
+		String[] arr3 = { "Car", "Review" };
+		String[] arr4 = { "Automobile" };
+		String[] arr5 = { "Car", "Ford" };
+		map.put("P1", arr1);
+		map.put("P2", arr2);
+		map.put("P3", arr3);
+		map.put("P4", arr4);
+		map.put("P5", arr5);
+		return map;
+	}
+
+	public String checkElement(String[] checkerElement) {
+		StringBuilder queryList = new StringBuilder();
+		Map<String, String[]> pagingStored = isPagingStored();
+		for (Map.Entry<String, String[]> entry : pagingStored.entrySet()) {
+			for (String elementCheck : checkerElement) {
+				if (Arrays.asList(entry.getValue()).contains(elementCheck)) {
+					queryList.append(entry.getKey());
+				}
+			}
+		}
+		if (queryList.toString().trim().equals("")) {
+			return "No Records Available";
+		}
+		return queryList.toString();
+
+	}
+
+}
